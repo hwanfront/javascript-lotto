a100b62ad18227dd7286561479c620633364bf88
hwanfront <kim015jh@gmail.com>
2022-12-12 16:34:29 +0900

    feat(LottoMachine): 로또 구매 메서드 추가

--- src/domain/LottoMachine.js
+++ src/domain/LottoMachine.js
@@ -1,5 +1,21 @@
-class LottoMachine {
+const Lotto = require('./Lotto');
+const RandomLottoGenerator = require('./RandomLottoGenerator');
 
+class LottoMachine {
+  static #PRICE = 1000;
+
+  static insert(money) {
+    const lottoCount = LottoMachine.#count(money);
+    return Array.from({ length: lottoCount }, LottoMachine.#create);
+  }
+
+  static #count(money) {
+    return Number(money) / LottoMachine.#PRICE;
+  }
+
+  static #create() {
+    return new Lotto(RandomLottoGenerator.generate());
+  }
 }
 
 module.exports = LottoMachine;
