7042a6ece70c4afbf7def6fcdad6581987441a33
hwanfront <kim015jh@gmail.com>
2022-12-12 17:11:58 +0900

    refactor(exception): 메시지 수정

--- src/exception/LottoDuplicatedException.js
+++ src/exception/LottoDuplicatedException.js
@@ -1,7 +1,7 @@
 const Exception = require('./Exception');
 
 class LottoDuplicatedException extends Exception {
-  static #ERROR_MESSAGE = '[ERROR]';
+  static #ERROR_MESSAGE = '로또 번호 중 중복된 값들이 있습니다.';
 
   constructor() {
     super(LottoDuplicatedException.#ERROR_MESSAGE);
--- src/exception/LottoSizeException.js
+++ src/exception/LottoSizeException.js
@@ -1,7 +1,7 @@
 const Exception = require('./Exception');
 
 class LottoSizeException extends Exception {
-  static #ERROR_MESSAGE = '로또 번호 중 중복된 값들이 있습니다.';
+  static #ERROR_MESSAGE = '로또 번호는 6개여야 합니다.';
 
   constructor() {
     super(LottoSizeException.#ERROR_MESSAGE);
