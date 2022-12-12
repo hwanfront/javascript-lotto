9f0e6d29e18cb26f594e6ec94c4211585bf24ef1
hwanfront <kim015jh@gmail.com>
2022-12-12 19:38:54 +0900

    feat(LottoResult): 당첨 결과에 개수를 추가하는 메서드 추가

--- src/domain/LottoResult.js
+++ src/domain/LottoResult.js
@@ -1,9 +1,25 @@
+const { PRIZE } = require('../static/constant');
+const Prize = require('./Prize');
+
 class LottoResult {
-  #prizes = new Map();
+  prizes = new Map();
 
-  calculateProfitRate() {
+  constructor() {
+    this.prizes = new Map(LottoResult.#initializePrizeMap());
+  }
 
+  static #initializePrizeMap() {
+    return Object.values(PRIZE).map((name) => [name, 0]);
+  }
+
+  calculatePrize({ count, hasBonus }) {
+    const prize = Prize.calculate({ count, hasBonus });
+    if (!this.prizes.has(prize)) {
+      return;
+    }
+
+    const prevPrizeCount = this.prizes.get(prize);
+    this.prizes.set(prize, prevPrizeCount + 1);
   }
 }
-
 module.exports = LottoResult;
