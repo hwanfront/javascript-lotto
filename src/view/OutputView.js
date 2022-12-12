a8df2bbc5ea2eddc75dbbd0f7ba01b8fefe3b9b3
hwanfront <kim015jh@gmail.com>
2022-12-12 20:43:01 +0900

    feat(OutputView): 출력 관련 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,28 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 const OutputView = {
 
+  message(type) {
+    return {
+      LOTTO_COUNT: '개를 구매했습니다.\n',
+      PRIZE_RESULT_TITLE: '당첨 통계\n---\n',
+      PROFIT_PREFIX: '총 수익률은',
+      PROFIT_SUFFIX: '%입니다.\n',
+    }[type] ?? '해당 없음';
+  },
+  printLottoCount(count) {
+    Console.print(`${count}${OutputView.message('LOTTO_COUNT')}`);
+  },
+  printLottos(lotto) {
+    Console.print(`${lotto}\n`);
+  },
+  printPrizeResult(prizes) {
+    Console.print(OutputView.message('PRIZE_RESULT_TITLE'));
+    Console.print(`${prizes}\n`);
+  },
+  printProfit(profit) {
+    Console.print(`${OutputView.message('PROFIT_PREFIX')} ${profit}${OutputView.message('PROFIT_SUFFIX')}`);
+  },
 };
 
 module.exports = OutputView;
