d4dd8cee2f2ab79f24bb73a43e3c162fb0424cec
hwanfront <kim015jh@gmail.com>
2022-12-13 19:39:39 +0900

    test(Lotto): 로또 메서드 관련 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,37 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require("../src/domain/Lotto");
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+  test.each([
+    [[1, 2, 3, 4, 5, 6], 1, true],
+    [[1, 2, 3, 4, 5, 6], 7, false]
+  ])("로또 번호에 숫자가 존재하는지 확인한다.", (numbers, number, result) => {
+    const lotto = new Lotto(numbers);
+    expect(lotto.has(number)).toEqual(result);
+  })
+
+  test.each([
+    [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 6],
+    [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], 0],
+    [[1, 2, 3, 4, 5, 6], [4, 5, 6, 7, 8, 9], 3]
+  ])("로또 번호들 끼리 같은 숫자가 몇 개 인지 확인한다.", (numbers1, numbers2, count) => {
+    const lotto1 = new Lotto(numbers1);
+    const lotto2 = new Lotto(numbers2);
+    expect(lotto1.countSameNumbers(lotto2)).toEqual(count);
+  })
+
+  test.each([
+    [[1, 2, 3, 4, 5, 6, 7]],
+    [[1, 2, 3, 4, 5]]
+  ])("로또 번호의 개수가 6개가 아니면 예외가 발생한다.", (numbers) => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      new Lotto(numbers);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
 });
