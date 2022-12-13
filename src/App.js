59a0a1bf11bba64f7a67ab3931218c30bd88372e
hwanfront <kim015jh@gmail.com>
2022-12-13 18:58:35 +0900

    feat(App): 애플리케이션 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const Controller = require('./controller/Controller');
+
 class App {
-  play() {}
+  #controller;
+
+  play() {
+    this.#controller = Controller.create();
+    this.#controller.start();
+  }
 }
 
 module.exports = App;
